e8b26131bde161d098437b02f999bf33e64be5c9
davicavalcanti1 <davicavalcantiaraujo1@gmail.com>
2026-02-19 18:19:26 -0300

    revert: remove farol_usg feature and migration

--- src/App.tsx
+++ src/App.tsx
@@ -53,7 +53,6 @@ import CilindrosForm from "./features/inspections/pages/forms/CilindrosForm";
 import CilindroConfirmacao from "./features/inspections/pages/CilindroConfirmacao";
 import DEAForm from "./features/inspections/pages/forms/DEAForm";
 import DEAPublicDetail from "./features/inspections/pages/DEAPublicDetail";
-import FarolUsg from "./pages/FarolUsg";
 
 
 
@@ -72,7 +71,6 @@ const App = () => (
             <Route path="/register" element={<Register />} />
             <Route path="/reset-password" element={<ResetPassword />} />
             <Route path="/paciente" element={<PublicPatientOccurrence />} />
-            <Route path="/ultrassom" element={<FarolUsg />} />
 
             {/* Cylinder Confirmation - Public Route (Moved to top for priority) */}
             <Route path="/inspecoes/cilindros/confirmar/:token" element={<CilindroConfirmacao />} />
